dbffbcd6467105c5f46e05a25af7455899a28ce9
Inochi Amaoto <inochiama@gmail.com>
2025-03-26 16:14:23 +0800

    aia: add aplic and imsic interrupt function
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- hw/spinal/aplic/AIAGeneric.scala
+++ hw/spinal/aplic/AIAGeneric.scala
@@ -3,94 +3,88 @@ package aplic
 import spinal.core._
 import spinal.lib._
 
-case class AIAInterrupteSource(sourceId: Int, priorityWidth : Int = 0) extends Area {
-  val id = sourceId
-  val prioWidth = if (priorityWidth > 0) Some(priorityWidth) else None
-  val prio = if (priorityWidth > 0) Some(UInt(priorityWidth bits)) else None
-  val ie = Bool
-  val ip = RegInit(False)
+abstract class AIARequest(idWidth: Int) extends Bundle {
+  val id = UInt(idWidth bits)
+  val valid = Bool()
+
+  def prioritize(other: AIARequest) : Bool
+  def pending(threshold: UInt) : Bool
 }
 
-case class AIAGeneric(interrupts: Seq[AIAInterrupteSource]) extends Area {
+abstract class AIAInterruptSource(sourceId: Int) extends Area {
+  val id = sourceId
+  val ie = Bool
+  val ip = RegInit(False)
+
+  def asRequest(idWidth : Int) : AIARequest
+}
+
+case class IMSICRequest(idWidth : Int) extends AIARequest(idWidth) {
+  override def prioritize(other: AIARequest): Bool = {
+    val x = other.asInstanceOf[IMSICRequest]
+    !x.valid || (valid && id <= x.id)
+  }
+
+  override def pending(threshold: UInt): Bool = {
+    valid && ((threshold === 0) || (id < threshold))
+  }
+}
+
+case class IMSICInterruptSource(sourceId : Int) extends AIAInterruptSource(sourceId) {
+  override def asRequest(idWidth : Int): AIARequest = {
+    val ret = new IMSICRequest(idWidth)
+    ret.id := U(id)
+    ret.valid := ip && ie
+    ret
+  }
+}
+
+case class APLICRequest(idWidth : Int, priorityWidth: Int) extends AIARequest(idWidth) {
+  val prio = UInt(priorityWidth bits)
+
+  override def prioritize(other: AIARequest): Bool = {
+    val x = other.asInstanceOf[APLICRequest]
+    !x.valid || (valid && ((prio < x.prio) || ((prio === x.prio) && (id <= x.id))))
+  }
+
+  override def pending(threshold: UInt): Bool = {
+    valid && ((threshold === 0) || (prio < threshold))
+  }
+}
+
+case class APLICInterruptSource(sourceId: Int, priorityWidth: Int) extends AIAInterruptSource(sourceId) {
+  val prio = UInt(priorityWidth bits)
+
+  override def asRequest(idWidth : Int): AIARequest = {
+    val ret = new APLICRequest(idWidth, priorityWidth)
+    ret.id := U(id)
+    ret.valid := ip && ie
+    ret.prio := prio
+    ret
+  }
+}
+
+case class AIAGeneric(interrupts: Seq[AIAInterruptSource]) extends Area {
   val maxSource = (interrupts.map(_.id) ++ Seq(0)).max + 1
   val idWidth = log2Up(maxSource)
   val threshold = UInt(idWidth bits)
-  // val prioWidth = (interrupts.map(_.prioWidth) ++ Seq(0)).max
-  val prioWidth = (interrupts.collect { case i if i.prioWidth.isDefined => i.prioWidth.get } ++ Seq(0)).max
 
-  abstract class RequestBase() extends Bundle{
-    val id = UInt(idWidth bits)
-    val valid = Bool()
+
+  val requests = interrupts.sortBy(_.id).map(g => g.asRequest(idWidth))
+
+  val bestRequest = RegNext(requests.reduceBalancedTree((a, b) => {
+    val takeA = a.prioritize(b)
+    takeA ? a | b
+  }))
+
+  val iep = bestRequest.pending(threshold)
+  val claim = iep ? bestRequest.id | 0
+
+  def doClaim(id: UInt) = new Area {
+    requests.find(_.id == id).map(_.valid := False)
   }
 
-  case class Request() extends RequestBase
-
-  case class RequestWithPriority() extends RequestBase{
-    val prio = UInt(prioWidth bits)
-  }
-
-  if (prioWidth > 0){
-    def Request(id : UInt, prio : UInt, valid : Bool) = {
-      val ret = new RequestWithPriority
-      ret.id := id
-      ret.valid := valid
-      ret.prio := prio
-      ret
-    }
-
-    val requests = Request(U(maxSource), Bits(prioWidth bits).setAll().asUInt, True) +: interrupts.sortBy(_.id).map(g =>
-      Request(
-        id       = U(g.id),
-        prio     = g.prio.get,
-        valid    = g.ip && g.ie
-      )
-    )
-
-    val bestRequest = RegNext(requests.reduceBalancedTree((a, b) => {
-      val takeA = !b.valid || (a.valid && a.prio <= b.prio)
-      takeA ? a | b
-    }))
-
-    val iep = (bestRequest.id < maxSource) && ((threshold === 0) || (bestRequest.prio < threshold))
-    val claim = iep ? bestRequest.id | 0
-
-    def doClaim(id: UInt) = new Area {
-      requests.find(_.id == id).map(_.valid := False)
-    }
-
-    def doBestClaim() = new Area {
-      doClaim(bestRequest.id)
-    }
-
-  }else{
-    def Request(id : UInt, valid : Bool) = {
-      val ret = new Request
-      ret.id := id
-      ret.valid := valid
-      ret
-    }
-
-    val requests = Request(U(maxSource), True) +: interrupts.sortBy(_.id).map(g =>
-      Request(
-        id       = U(g.id),
-        valid    = g.ip && g.ie
-      )
-    )
-
-    val bestRequest = RegNext(requests.reduceBalancedTree((a, b) => {
-      val takeA = !b.valid || (a.valid && a.id <= b.id)
-      takeA ? a | b
-    }))
-
-    val iep = (bestRequest.id < maxSource) && ((threshold === 0) || (bestRequest.id < threshold))
-    val claim = iep ? bestRequest.id | 0
-
-    def doClaim(id: UInt) = new Area {
-      requests.find(_.id == id).map(_.valid := False)
-    }
-
-    def doBestClaim() = new Area {
-      doClaim(bestRequest.id)
-    }
+  def doBestClaim() = new Area {
+    doClaim(bestRequest.id)
   }
 }
