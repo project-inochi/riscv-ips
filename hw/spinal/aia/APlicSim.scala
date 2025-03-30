45d7001800396b6c1b4a17d431d22e57dd480b6f
Inochi Amaoto <inochiama@gmail.com>
2025-03-30 12:50:03 +0800

    hw: sim: simplify UINT32 calling
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- hw/spinal/aia/APlicSim.scala
+++ hw/spinal/aia/APlicSim.scala
@@ -4,7 +4,7 @@ import spinal.core._
 import spinal.core.sim._
 import spinal.lib.bus.tilelink
 import config.Config
-import _root_.sim.simTools
+import _root_.sim._
 
 object APlicSim extends App {
 
@@ -42,63 +42,63 @@ object APlicSim extends App {
     implicit val idAllocator = new tilelink.sim.IdAllocator(tilelink.DebugId.width)
     val agent = new tilelink.sim.MasterAgent(dut.io.bus, dut.clockDomain)
 
-    val prioArray = Seq(simTools.UInt32(0x1),
-                        simTools.UInt32(0x1),
-                        simTools.UInt32(0x4),
-                        simTools.UInt32(0x5),
-                        simTools.UInt32(0x6),
-                        simTools.UInt32(0x6),
-                        simTools.UInt32(0x7))
+    val prioArray = Seq(SimUInt32(0x1),
+                        SimUInt32(0x1),
+                        SimUInt32(0x4),
+                        SimUInt32(0x5),
+                        SimUInt32(0x6),
+                        SimUInt32(0x6),
+                        SimUInt32(0x7))
     for (i <- 1 until sourcenum){
       print(agent.putFullData(0, aplicmap.sourcecfgOffset + (i << aplicmap.idShift), prioArray(i-1)))
     }
 
-    print(agent.putFullData(0, aplicmap.setieOffset, simTools.UInt32(0xffffffff)))
+    print(agent.putFullData(0, aplicmap.setieOffset, SimUInt32(0xffffffff)))
 
-    val targetArray = Seq(simTools.UInt32(0x1),
-                          simTools.UInt32(0x2),
-                          simTools.UInt32(0x3),
-                          simTools.UInt32(0x4),
-                          simTools.UInt32(0x5),
-                          simTools.UInt32(0x6),
-                          simTools.UInt32(0x40007))
+    val targetArray = Seq(SimUInt32(0x1),
+                          SimUInt32(0x2),
+                          SimUInt32(0x3),
+                          SimUInt32(0x4),
+                          SimUInt32(0x5),
+                          SimUInt32(0x6),
+                          SimUInt32(0x40007))
     for (i <- 1 until sourcenum){
       print(agent.putFullData(0, aplicmap.targetOffset + (i << aplicmap.idShift), targetArray(i-1)))
     }
 
-    val setipdata = simTools.UInt32((0x0))
+    val setipdata = SimUInt32(0x0)
     print(agent.putFullData(0, aplicmap.setipOffset, setipdata))
 
-    val domaincfgdata = simTools.UInt32(0x80000100)
+    val domaincfgdata = SimUInt32(0x80000100)
     print(agent.putFullData(0, aplicmap.domaincfgOffset, domaincfgdata))
 
     // set/clripnum
-    print(agent.putFullData(0, aplicmap.setipnumOffset, simTools.UInt32(0x1)))
+    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x1)))
     dut.clockDomain.waitRisingEdge(10)
-    print(agent.putFullData(0, aplicmap.setipnumOffset, simTools.UInt32(0x2)))
+    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x2)))
     dut.clockDomain.waitRisingEdge(10)
-    print(agent.putFullData(0, aplicmap.clripnumOffset, simTools.UInt32(0x1)))
+    print(agent.putFullData(0, aplicmap.clripnumOffset, SimUInt32(0x1)))
     print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
 
     // setip and the block of setip(num) when mode is high or low
-    print(agent.putFullData(0, aplicmap.setipnumOffset, simTools.UInt32(0x5)))
-    print(agent.putFullData(0, aplicmap.setipnumOffset, simTools.UInt32(0x6)))
-    print(agent.putFullData(0, aplicmap.setipnumOffset, simTools.UInt32(0x7)))
-    print(agent.putFullData(0, aplicmap.setipOffset, simTools.UInt32(0b11111100)))
+    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x5)))
+    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x6)))
+    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x7)))
+    print(agent.putFullData(0, aplicmap.setipOffset, SimUInt32(0b11111100)))
     print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, simTools.UInt32(0x1)))
-    print(agent.putFullData(0, aplicmap.setipOffset, simTools.UInt32(0b1110001)))
+    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, SimUInt32(0x1)))
+    print(agent.putFullData(0, aplicmap.setipOffset, SimUInt32(0b1110001)))
     print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
     print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
     print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, simTools.UInt32(0x6)))
+    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, SimUInt32(0x6)))
 
     // input interupt
-    print(agent.putFullData(0, aplicmap.clrienumOffset, simTools.UInt32(0x1)))
+    print(agent.putFullData(0, aplicmap.clrienumOffset, SimUInt32(0x1)))
     dut.io.sources #= 0b0111111
     dut.clockDomain.waitRisingEdge(10)
     dut.io.sources #= 0b0110111
-    print(agent.putFullData(0, aplicmap.setienumOffset, simTools.UInt32(0x1)))
+    print(agent.putFullData(0, aplicmap.setienumOffset, SimUInt32(0x1)))
     dut.clockDomain.waitRisingEdge(10)
     for (i <- 0 until 6){
       print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
---
+++ hw/spinal/sim/busHelper.scala
@@ -0,0 +1,32 @@
+package sim
+
+import spinal.core._
+import spinal.core.sim._
+
+class SimUIntFix(width: Int)(number: BigInt, endian: Endianness = LITTLE) {
+  val bits = width
+  val value = number
+  val endianness = endian
+}
+
+object SimUIntFix {
+  implicit def simUintFix2SeqByte(intx: SimUIntFix): Seq[Byte] = {
+    intx.value.toBytes(intx.bits, intx.endianness)
+  }
+}
+
+object SimUInt8 {
+  def apply(number: BigInt, endian: Endianness = LITTLE): SimUIntFix = new SimUIntFix(8)(number, endian)
+}
+
+object SimUInt16 {
+  def apply(number: BigInt, endian: Endianness = LITTLE): SimUIntFix = new SimUIntFix(16)(number, endian)
+}
+
+object SimUInt32 {
+  def apply(number: BigInt, endian: Endianness = LITTLE): SimUIntFix = new SimUIntFix(32)(number, endian)
+}
+
+object SimUInt64 {
+  def apply(number: BigInt, endian: Endianness = LITTLE): SimUIntFix = new SimUIntFix(64)(number, endian)
+}
