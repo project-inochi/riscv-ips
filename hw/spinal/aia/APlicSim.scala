9befcee380b9134d4440cc6994758117ac2bc365
Longbin Li <looong.bin@gmail.com>
2025-04-02 17:55:43 +0800

    hw: aia: aplic: simulation with delegation
    Signed-off-by: Longbin Li <looong.bin@gmail.com>

--- hw/spinal/aia/APlicSim.scala
+++ hw/spinal/aia/APlicSim.scala
@@ -1,13 +1,13 @@
 package aia
 
 import spinal.core._
+import spinal.lib._
 import spinal.core.sim._
 import spinal.lib.bus.tilelink
 import config.Config
 import _root_.sim._
 
 object APlicSim extends App {
-
   val sourcenum = 8
   val hartnum = 2
 
@@ -17,7 +17,96 @@ object APlicSim extends App {
   val aplicmap = APlicMapping.aplicMap
 
   val compile = config.Config.sim.compile{
-    val aplic = new TilelinkAplic(sourceIds, hartIds, Seq(),
+    val aplics = new aplics()
+    aplics
+  }
+
+  compile.doSim{ dut =>
+    dut.clockDomain.forkStimulus(10)
+
+    dut.io.sources #= 0b1000001
+
+    implicit val idAllocator = new tilelink.sim.IdAllocator(tilelink.DebugId.width)
+    val agentmaster = new tilelink.sim.MasterAgent(dut.io.busmaster, dut.clockDomain)
+    val agentslave = new tilelink.sim.MasterAgent(dut.io.busslave, dut.clockDomain)
+
+    val prioArrayM = Seq(SimUInt32(0x400),
+                        SimUInt32(0x1),
+                        SimUInt32(0x4),
+                        SimUInt32(0x400),
+                        SimUInt32(0x5),
+                        SimUInt32(0x6),
+                        SimUInt32(0x7))
+    val prioArrayS = Seq(SimUInt32(0x7),
+                        SimUInt32(0x0),
+                        SimUInt32(0x0),
+                        SimUInt32(0x5),
+                        SimUInt32(0x0),
+                        SimUInt32(0x0),
+                        SimUInt32(0x0))
+    for (i <- 1 until sourcenum){
+      print(agentmaster.putFullData(0, aplicmap.sourcecfgOffset + (i << aplicmap.idShift), prioArrayM(i-1)))
+      print(agentslave.putFullData(0, aplicmap.sourcecfgOffset + (i << aplicmap.idShift), prioArrayS(i-1)))
+    }
+
+    print(agentmaster.putFullData(0, aplicmap.setieOffset, SimUInt32(0xff)))
+    print(agentslave.putFullData(0, aplicmap.setieOffset, SimUInt32(0xff)))
+
+    val targetArray = Seq(SimUInt32(0x1),
+                          SimUInt32(0x2),
+                          SimUInt32(0x3),
+                          SimUInt32(0x4),
+                          SimUInt32(0x5),
+                          SimUInt32(0x6),
+                          SimUInt32(0x40007))
+    for (i <- 1 until sourcenum){
+      print(agentmaster.putFullData(0, aplicmap.targetOffset + (i << aplicmap.idShift), targetArray(i-1)))
+      print(agentslave.putFullData(0, aplicmap.targetOffset + (i << aplicmap.idShift), targetArray(i-1)))
+    }
+
+		print(agentmaster.putFullData(0, aplicmap.setipOffset, SimUInt32(0x0)))
+		print(agentslave.putFullData(0, aplicmap.setipOffset, SimUInt32(0x0)))
+
+		print(agentmaster.putFullData(0, aplicmap.domaincfgOffset, SimUInt32(0x80000100)))
+		print(agentslave.putFullData(0, aplicmap.domaincfgOffset, SimUInt32(0x80000100)))
+
+    // set/clripnum
+    print(agentmaster.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x2)))
+    assertState(dut.aplicmaster.interrupts(1).ip, true, "master")
+    print(agentmaster.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x1)))
+    assertState(dut.aplicmaster.interrupts(0).ip, false, "master")
+
+    print(agentmaster.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
+    assertState(dut.aplicmaster.interrupts(1).ip, false, "master")
+    dut.clockDomain.waitRisingEdge(10)
+
+    // setip and the block of setip(num) when mode is high or low
+    print(agentmaster.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x5)))
+    print(agentmaster.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x6)))
+    print(agentmaster.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x7)))
+    print(agentmaster.putFullData(0, aplicmap.setipOffset, SimUInt32(0b00001100)))
+    for (i <- 0 until 6){
+      print(agentmaster.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
+    }
+
+    // input and delagation
+    dut.io.sources #= 0b0111110
+    dut.clockDomain.waitRisingEdge(10)
+    dut.io.sources #= 0b0100111
+    dut.clockDomain.waitRisingEdge(10)
+    for (i <- 0 until 6){
+      print(agentmaster.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
+      print(agentslave.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
+    }
+    print(agentmaster.get(0, aplicmap.idcOffset + aplicmap.idcGroup +aplicmap.claimiOffset, 4))
+
+    dut.io.sources #= 0b1000001
+    //end
+    dut.clockDomain.waitRisingEdge(10)
+  }
+
+  class aplics() extends Component {
+    val aplicslave = new TilelinkAplic(sourceIds, hartIds, Seq(),
       tilelink.M2sParameters(
         sourceCount = 1,
         support = tilelink.M2sSupport(
@@ -31,83 +120,59 @@ object APlicSim extends App {
       ).toNodeParameters().toBusParameter()
     )
 
-    aplic
+    val aplicmaster = new TilelinkAplic(sourceIds, hartIds, Seq(aplicslave),
+      tilelink.M2sParameters(
+        sourceCount = 1,
+        support = tilelink.M2sSupport(
+          addressWidth = 16,
+          dataWidth = 32,
+          transfers = tilelink.M2sTransfers(
+            get = tilelink.SizeRange(8),
+            putFull = tilelink.SizeRange(8)
+          )
+        )
+      ).toNodeParameters().toBusParameter()
+    )
+
+    aplicmaster.interrupts.foreach(_.ip.simPublic())
+    aplicslave.interrupts.foreach(_.ip.simPublic())
+
+    val io = new Bundle {
+      val busmaster = slave(new bus.tilelink.Bus(tilelink.M2sParameters(
+        sourceCount = 1,
+        support = tilelink.M2sSupport(
+          addressWidth = 16,
+          dataWidth = 32,
+          transfers = tilelink.M2sTransfers(
+            get = tilelink.SizeRange(8),
+            putFull = tilelink.SizeRange(8)
+          )
+        )
+      ).toNodeParameters().toBusParameter()))
+      val busslave = slave(new bus.tilelink.Bus(tilelink.M2sParameters(
+        sourceCount = 1,
+        support = tilelink.M2sSupport(
+          addressWidth = 16,
+          dataWidth = 32,
+          transfers = tilelink.M2sTransfers(
+            get = tilelink.SizeRange(8),
+            putFull = tilelink.SizeRange(8)
+          )
+        )
+      ).toNodeParameters().toBusParameter()))
+      val sources = in Bits (7 bits)
+      val targetsmaster = out Bits (2 bits)
+      val targetsslave = out Bits (2 bits)
+    }
+    io.busmaster <> aplicmaster.io.bus
+    io.busslave <> aplicslave.io.bus
+    aplicmaster.io.sources := io.sources
+    io.targetsmaster := aplicmaster.io.targets
+    io.targetsslave := aplicslave.io.targets
+    aplicslave.io.sources := aplicmaster.io.slaveSources(0)
   }
 
-  compile.doSim{ dut =>
-    dut.clockDomain.forkStimulus(10)
-
-    dut.io.sources #= 0b1000000
-
-    implicit val idAllocator = new tilelink.sim.IdAllocator(tilelink.DebugId.width)
-    val agent = new tilelink.sim.MasterAgent(dut.io.bus, dut.clockDomain)
-
-    val prioArray = Seq(SimUInt32(0x1),
-                        SimUInt32(0x1),
-                        SimUInt32(0x4),
-                        SimUInt32(0x5),
-                        SimUInt32(0x6),
-                        SimUInt32(0x6),
-                        SimUInt32(0x7))
-    for (i <- 1 until sourcenum){
-      print(agent.putFullData(0, aplicmap.sourcecfgOffset + (i << aplicmap.idShift), prioArray(i-1)))
-    }
-
-    print(agent.putFullData(0, aplicmap.setieOffset, SimUInt32(0xffffffff)))
-
-    val targetArray = Seq(SimUInt32(0x1),
-                          SimUInt32(0x2),
-                          SimUInt32(0x3),
-                          SimUInt32(0x4),
-                          SimUInt32(0x5),
-                          SimUInt32(0x6),
-                          SimUInt32(0x40007))
-    for (i <- 1 until sourcenum){
-      print(agent.putFullData(0, aplicmap.targetOffset + (i << aplicmap.idShift), targetArray(i-1)))
-    }
-
-    val setipdata = SimUInt32(0x0)
-    print(agent.putFullData(0, aplicmap.setipOffset, setipdata))
-
-    val domaincfgdata = SimUInt32(0x80000100)
-    print(agent.putFullData(0, aplicmap.domaincfgOffset, domaincfgdata))
-
-    // set/clripnum
-    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x1)))
-    dut.clockDomain.waitRisingEdge(10)
-    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x2)))
-    dut.clockDomain.waitRisingEdge(10)
-    print(agent.putFullData(0, aplicmap.clripnumOffset, SimUInt32(0x1)))
-    print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-
-    // setip and the block of setip(num) when mode is high or low
-    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x5)))
-    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x6)))
-    print(agent.putFullData(0, aplicmap.setipnumOffset, SimUInt32(0x7)))
-    print(agent.putFullData(0, aplicmap.setipOffset, SimUInt32(0b11111100)))
-    print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, SimUInt32(0x1)))
-    print(agent.putFullData(0, aplicmap.setipOffset, SimUInt32(0b1110001)))
-    print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    print(agent.putFullData(0, aplicmap.sourcecfgOffset + 20, SimUInt32(0x6)))
-
-    // input interupt
-    print(agent.putFullData(0, aplicmap.clrienumOffset, SimUInt32(0x1)))
-    dut.io.sources #= 0b0111111
-    dut.clockDomain.waitRisingEdge(10)
-    dut.io.sources #= 0b0110111
-    print(agent.putFullData(0, aplicmap.setienumOffset, SimUInt32(0x1)))
-    dut.clockDomain.waitRisingEdge(10)
-    for (i <- 0 until 6){
-      print(agent.get(0, aplicmap.idcOffset + aplicmap.claimiOffset, 4))
-    }
-      print(agent.get(0, aplicmap.idcOffset + aplicmap.idcGroup +aplicmap.claimiOffset, 4))
-
-    dut.io.sources #= 0b1000000
-
-    //end
-    dut.clockDomain.waitRisingEdge(10)
+  def assertState(signal : Bool, state : Boolean, name : String) : Unit = {
+    assert(signal.toBoolean == state, s"$name: missmatch (${signal} != ${state.toString()})")
   }
 }
