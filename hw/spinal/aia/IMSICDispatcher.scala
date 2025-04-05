6fd10e4371723212e1fa7f82352aab844fc4cf9d
Inochi Amaoto <inochiama@gmail.com>
2025-04-05 20:12:34 +0800

    hw: aia: imsic: update IMSIC dispatcher mapping calculation
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- hw/spinal/aia/IMSICDispatcher.scala
+++ hw/spinal/aia/IMSICDispatcher.scala
@@ -7,39 +7,17 @@ import aia.APlicSim.hartIds
 
 case class IMSICDispatcherMapping(
   numberGuest                 : Int,
-  interruptFileSize           : BigInt,
-  interruptFileOffset         : BigInt,
-  interruptFileGroupSize      : BigInt,
+  interruptFileHartSize       : BigInt = 0,
+  interruptFileHartOffset     : BigInt = 0,
+  interruptFileGroupSize      : BigInt = 0,
 )
 
 object IMSICDispatcherMapping {
-  val r1g0 = IMSICDispatcherMapping(
-    numberGuest               = 0,
-    interruptFileSize         = 0,
-    interruptFileOffset       = 0,
-    interruptFileGroupSize    = 0,
-  )
-
-  val r1g1 = IMSICDispatcherMapping(
-    numberGuest               = 1,
-    interruptFileSize         = 0,
-    interruptFileOffset       = 0,
-    interruptFileGroupSize    = 0,
-  )
-
-  val r1g7 = IMSICDispatcherMapping(
-    numberGuest               = 7,
-    interruptFileSize         = 0,
-    interruptFileOffset       = 0,
-    interruptFileGroupSize    = 0,
-  )
-
-  val r1g15 = IMSICDispatcherMapping(
-    numberGuest               = 15,
-    interruptFileSize         = 0,
-    interruptFileOffset       = 0,
-    interruptFileGroupSize    = 0,
-  )
+  val r1g0 = IMSICDispatcherMapping(numberGuest = 0)
+  val r1g1 = IMSICDispatcherMapping(numberGuest = 1)
+  val r1g3 = IMSICDispatcherMapping(numberGuest = 3)
+  val r1g7 = IMSICDispatcherMapping(numberGuest = 7)
+  val r1g15 = IMSICDispatcherMapping(numberGuest = 15)
 }
 
 case class IMSICDispatcherInfo(
@@ -49,29 +27,33 @@ case class IMSICDispatcherInfo(
 )
 
 object IMSICDispatcher {
-  val interuptFileSize: BigInt = 4096
+  val interruptFileSize: BigInt = 4096
 
   def apply(bus: BusSlaveFactory, mapping : IMSICDispatcherMapping)(infos : Seq[IMSICDispatcherInfo]) = new Area {
     import mapping._
 
     require(numberGuest < 16, "Per hart can only have max 15 guest interrupt files.")
-    require(interruptFileSize == 0 || isPow2(interruptFileSize), "interruptFileSize should be power of 2")
+    require(interruptFileHartSize == 0 || isPow2(interruptFileHartSize), "interruptFileHartSize should be power of 2")
     require(interruptFileGroupSize == 0 || isPow2(interruptFileGroupSize), "interruptFileGroupSize should be power of 2")
 
-    val realInterruptFileSize = if (interruptFileSize != 0) interruptFileSize else interuptFileSize
-    val interruptFileNumber =  1 << log2Up(infos.map(_.sources.guestId).max + 1)
-    val interruptFileMask = interruptFileNumber - 1
+    val intFileNumber = 1 << log2Up(infos.map(_.sources.guestId).max + 1)
+    val minIntFileHartSize = interruptFileSize * intFileNumber
+    val realIntFileHartSize = if (interruptFileHartSize != 0) interruptFileHartSize else minIntFileHartSize
+    require(realIntFileHartSize >= minIntFileHartSize)
 
-    val realInterruptFileGroupSize = if (interruptFileGroupSize != 0) interruptFileGroupSize else realInterruptFileSize * interruptFileNumber
+    val intFileGroupHarts = 1 << log2Up(infos.map(_.groupHartId).max + 1)
+    val minIntFileGroupSize = realIntFileHartSize * intFileGroupHarts
+    val realIntFileGroupSize = if (interruptFileGroupSize != 0) interruptFileGroupSize else minIntFileGroupSize
+    require(realIntFileGroupSize >= minIntFileGroupSize)
 
-    val interruptFileGroupNumber = infos.map(_.groupId).max + 1
-    val interruptFileGroupMask = 1 << log2Up(interruptFileGroupNumber) - 1
-    val interruptTestMask = interruptFileMask * realInterruptFileSize + interruptFileGroupMask * realInterruptFileGroupSize
-    require((interruptTestMask & interruptFileOffset) != 0, "interruptFileOffset should not cover any interrupt file")
+    val intFileGroupMask = minIntFileGroupSize - 1
+    val intFileGroupIdMask = 1 << log2Up(infos.map(_.groupId).max + 1) - 1
+    val intFileTestMask = intFileGroupMask + realIntFileGroupSize * intFileGroupIdMask
+    require((interruptFileHartOffset & intFileTestMask) != 0, "interruptFileHartOffset should not cover any interrupt file")
 
     for (info <- infos) yield new Area {
       val imsic = IMSIC(info.sources)
-      val offset = info.groupId * interruptFileGroupSize + info.groupHartId * interruptFileGroupSize + interruptFileOffset + info.sources.guestId * realInterruptFileSize
+      val offset = info.groupId * realIntFileGroupSize + info.groupHartId * realIntFileHartSize + interruptFileHartOffset + info.sources.guestId * interruptFileSize
 
       imsic.driveFrom(bus, offset)
     }
