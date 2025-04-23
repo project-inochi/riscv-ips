3d72d8b9767d45c6e5f52bd112ef19dd7c33f235
Longbin Li <looong.bin@gmail.com>
2025-04-23 18:37:17 +0800

    test: aplic: sim: success compile
    Signed-off-by: Longbin Li <looong.bin@gmail.com>

---
+++ test/spinal/aia/APlicTest.scala
@@ -0,0 +1,32 @@
+package aia
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.sim._
+import spinal.lib._
+import spinal.lib.bus.tilelink
+import spinal.tester.{SpinalAnyFunSuite, SpinalSimTester}
+import _root_.sim._
+
+class APlicTest extends SpinalAnyFunSuite {
+  var compile: SimCompiled[TilelinkAPLICFiberTest] = null
+
+  val sourcenum = 8
+  val hartnum = 2
+
+  val sourceIds = for (i <- 1 until sourcenum) yield i
+  val slavesourceIds = IndexedSeq(1, 4)
+  val hartIds = for (i <- 0 until hartnum) yield i
+
+  val aplicmap = APlicMapping
+
+  def doCompile(): Unit ={
+      compile = config.Config.sim.compile(
+      new TilelinkAPLICFiberTest(hartIds, sourceIds, slavesourceIds)
+      )
+  }
+
+  test("compile") {
+      doCompile()
+  }
+}
