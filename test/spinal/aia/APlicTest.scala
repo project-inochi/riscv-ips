987d3792aef92c46990f76d5890ce53aabb18919
Longbin Li <looong.bin@gmail.com>
2025-04-27 19:26:53 +0800

    test: aia: aplic: add master-slave check point for aplic
    Signed-off-by: Longbin Li <looong.bin@gmail.com>

--- test/spinal/aia/APlicTest.scala
+++ test/spinal/aia/APlicTest.scala
@@ -71,6 +71,91 @@ case class APlicUnitFiberTest(hartIds: Seq[Int], sourceIds: Seq[Int]) extends Co
   io.ip := Vec(blocks.map(block => block.interrupts.map(_.ip).asBits()))
 }
 
+case class APlicMSFiberTest(hartIds: Seq[Int], sourceIds: Seq[Int], slavesourceIds: Seq[Int]) extends Component {
+  val masterBus = TilelinkBusFiber()
+
+  val crossBar = tilelink.fabric.Node()
+  crossBar << masterBus.node
+
+  val slaveInfos = Seq(APlicSlaveInfo(1, slavesourceIds), APlicSlaveInfo(2, slavesourceIds))
+
+  val peripherals = new Area {
+    val access = tilelink.fabric.Node()
+    access << crossBar
+
+    val S1 = TilelinkAPLICFiber()
+    S1.up at 0x10000000 of access
+    crossBar << S1.down
+
+    val S2 = TilelinkAPLICFiber()
+    S2.up at 0x20000000 of access
+    crossBar << S2.down
+
+    val M = TilelinkAPLICFiber()
+    M.up at 0x30000000 of access
+    crossBar << M.down
+
+    M.domainParam = Some(APlicDomainParam.root(APlicGenParam.full))
+    S1.domainParam = Some(APlicDomainParam.S(APlicGenParam.full))
+    S2.domainParam = Some(APlicDomainParam.S(APlicGenParam.full))
+
+    val targets1SBundles = hartIds.map(hartId => {
+      val node = InterruptNode.slave()
+      S1.mapDownInterrupt(hartId, node)
+      node
+    })
+
+    val targets2SBundles = hartIds.map(hartId => {
+      val node = InterruptNode.slave()
+      S2.mapDownInterrupt(hartId, node)
+      node
+    })
+
+    val targetsMBundles = hartIds.map(hartId => {
+      val node = InterruptNode.slave()
+      M.mapDownInterrupt(hartId, node)
+      node
+    })
+
+    val sourcesMBundles = sourceIds.map(sourceId => {
+      val node = InterruptNode.master()
+      M.mapUpInterrupt(sourceId, node)
+      node
+    })
+
+    val slaveSources = slaveInfos.map(M.createInterruptDelegation(_))
+
+    // XXX: there is only one slave
+    val sourcesS1Bundles = slavesourceIds.zip(slaveSources(0).flags).map {
+      case (id, slaveSource) => S1.mapUpInterrupt(id, slaveSource)
+    }
+    val sourcesS2Bundles = slavesourceIds.zip(slaveSources(1).flags).map {
+      case (id, slaveSource) => S2.mapUpInterrupt(id, slaveSource)
+    }
+
+    S1.mmsiaddrcfg := M.mmsiaddrcfg
+    S1.smsiaddrcfg := M.smsiaddrcfg
+    S2.mmsiaddrcfg := M.mmsiaddrcfg
+    S2.smsiaddrcfg := M.smsiaddrcfg
+  }
+
+  val io = new Bundle {
+    val bus = slave(tilelink.Bus(masterBus.m2sParams.toNodeParameters().toBusParameter()))
+    val sources = in Bits(sourceIds.size bits)
+    val targetsmaster = out Bits(hartIds.size bits)
+    val targets1slave = out Bits(hartIds.size bits)
+    val targets2slave = out Bits(hartIds.size bits)
+  }
+
+  masterBus.bus = Some(io.bus)
+
+  peripherals.sourcesMBundles.lazyZip(io.sources.asBools).foreach(_.flag := _)
+
+  io.targetsmaster := peripherals.targetsMBundles.map(_.flag).asBits()
+  io.targets1slave := peripherals.targets1SBundles.map(_.flag).asBits()
+  io.targets2slave := peripherals.targets2SBundles.map(_.flag).asBits()
+}
+
 class APlicUnitTest extends APlicTest {
   var compile: SimCompiled[APlicUnitFiberTest] = null
 
@@ -86,10 +171,10 @@ class APlicUnitTest extends APlicTest {
     )
   }
 
-  test("APlic Direct") {
+  test("Direct") {
     doCompile()
 
-    compile.doSim("APlic Direct"){ dut =>
+    compile.doSim("Direct"){ dut =>
       dut.clockDomain.forkStimulus(10)
 
       dut.io.sources #= 0x0
@@ -171,13 +256,16 @@ class APlicUnitTest extends APlicTest {
       }
       // source => setip 4.5.5
       var sourceIO = BigInt("0", 16)
-      assertIP(dut, sourceIO, configs)
+      dut.io.sources #= sourceIO
+      assertIP(sourceIO, configs)
 
       sourceIO = BigInt("7fffffffffffffff", 16)
-      assertIP(dut, sourceIO, configs)
+      dut.io.sources #= sourceIO
+      assertIP(sourceIO, configs)
 
       sourceIO = BigInt("0", 16)
-      assertIP(dut, sourceIO, configs)
+      dut.io.sources #= sourceIO
+      assertIP(sourceIO, configs)
 
       for ((config, i) <- configs.zipWithIndex) {
         if (config.mode == sourceMode.LEVEL0) {
@@ -243,10 +331,10 @@ class APlicUnitTest extends APlicTest {
     }
   }
 
-  test("APlic MSI") {
+  test("MSI") {
     doCompile()
 
-    compile.doSim("APlic MSI"){ dut =>
+    compile.doSim("MSI"){ dut =>
       dut.clockDomain.forkStimulus(10)
 
       dut.io.sources #= 0x0
@@ -311,6 +399,126 @@ class APlicUnitTest extends APlicTest {
   }
 }
 
+class APlicMSTest extends APlicTest {
+  var compile: SimCompiled[APlicMSFiberTest] = null
+
+  val sourcenum = 64
+  val hartnum = 8
+
+  val sourceIds = for (i <- 1 until sourcenum) yield i
+  val slavesourceIds = (1 to 63).toIndexedSeq
+  val hartIds = for (i <- 0 until hartnum) yield i
+
+  def doCompile(): Unit ={
+    compile = SimConfig.withConfig(config.TestConfig.spinal).withFstWave.compile(
+      new APlicMSFiberTest(hartIds, sourceIds, slavesourceIds)
+    )
+  }
+
+  test("MS Direct") {
+    doCompile()
+
+    compile.doSim("MS Direct"){ dut =>
+      dut.clockDomain.forkStimulus(10)
+
+      dut.io.sources #= 0x0
+
+      implicit val idAllocator = new tilelink.sim.IdAllocator(tilelink.DebugId.width)
+      val agent = new tilelink.sim.MasterAgent(dut.io.bus, dut.clockDomain)
+    
+      val masterAddr = 0x30000000
+      val slave1Addr = 0x10000000
+      val slave2Addr = 0x20000000
+
+      agent.putFullData(0, masterAddr + aplicmap.domaincfgOffset, SimUInt32(0x80000000))
+      agent.putFullData(0, slave1Addr + aplicmap.domaincfgOffset, SimUInt32(0x80000000))
+      agent.putFullData(0, slave2Addr + aplicmap.domaincfgOffset, SimUInt32(0x80000000))
+
+      val candidates = (1 to 63).toSet
+      val shuffledCandidates = Random.shuffle(candidates.toList).take(32)
+
+      val randomIds_slave1 = shuffledCandidates.take(16)
+      val randomIds_slave2 = shuffledCandidates.drop(16)
+      print(randomIds_slave1)
+      print("\n"+randomIds_slave2)
+
+      val masterconfigs = ArrayBuffer[gateway]()
+      for (i <- 1 until sourcenum) {
+        val isDelegaton = randomIds_slave1.contains(i) || randomIds_slave2.contains(i)
+        val mode = if (isDelegaton) sourceMode.INACTIVE else sourceMode.random()
+        val config = createGateway(mode, i, agent, masterAddr)
+        config.hartId = Random.nextInt(hartnum)
+        config.iprio = 1
+        // need simplify
+        config.setMode(agent, masterAddr, (i-1)*4, if (isDelegaton) {if (randomIds_slave1.contains(i)) 1 else 2} else 0)
+        masterconfigs += config
+      }
+
+      val slave1configs = ArrayBuffer[gateway]()
+      val slave2configs = ArrayBuffer[gateway]()
+      for (i <- candidates) {
+        // need simplify
+        var isDelegaton = randomIds_slave1.contains(i)
+        var mode = if (isDelegaton) sourceMode.LEVEL0 else sourceMode.INACTIVE
+        
+        val slave1config = createGateway(mode, i, agent, slave1Addr)
+        slave1config.hartId = Random.nextInt(hartnum)
+        slave1config.iprio = 1
+        slave1config.setMode(agent, slave1Addr, (i-1)*4, 0)
+        slave1configs += slave1config
+
+        isDelegaton = randomIds_slave2.contains(i)
+        mode = if (isDelegaton) sourceMode.LEVEL0 else sourceMode.INACTIVE
+
+        val slave2config = createGateway(mode, i, agent, slave2Addr)
+        slave2config.hartId = Random.nextInt(hartnum)
+        slave2config.iprio = 1
+        slave2config.setMode(agent, slave2Addr, (i-1)*4, 0)
+        slave2configs += slave2config
+      }
+
+      for (i <- 0 until hartnum) {
+        agent.putFullData(0, masterAddr + aplicmap.idcOffset + aplicmap.ideliveryOffset + i * aplicmap.idcGroupSize, SimUInt32(0x1))
+        agent.putFullData(0, slave1Addr + aplicmap.idcOffset + aplicmap.ideliveryOffset + i * aplicmap.idcGroupSize, SimUInt32(0x1))
+        agent.putFullData(0, slave2Addr + aplicmap.idcOffset + aplicmap.ideliveryOffset + i * aplicmap.idcGroupSize, SimUInt32(0x1))
+      }
+
+      agent.putFullData(0, masterAddr + aplicmap.domaincfgOffset, SimUInt32(0x80000100))
+      agent.putFullData(0, slave1Addr + aplicmap.domaincfgOffset, SimUInt32(0x80000100))
+      agent.putFullData(0, slave2Addr + aplicmap.domaincfgOffset, SimUInt32(0x80000100))
+
+      dut.clockDomain.waitRisingEdge(10)
+
+      var sourceIO = BigInt("0", 16)
+      dut.io.sources #= sourceIO
+      assertIP(sourceIO, masterconfigs)
+
+      // modify
+      assertIP(sourceIO, slave1configs)
+      assertIP(sourceIO, slave2configs)
+      dut.clockDomain.waitRisingEdge(10)
+
+      sourceIO = BigInt("7fffffffffffffff", 16)
+      dut.io.sources #= sourceIO
+      assertIP(sourceIO, masterconfigs)
+
+      assertIP(sourceIO, slave1configs)
+      assertIP(sourceIO, slave2configs)
+
+      dut.clockDomain.waitRisingEdge(10)
+
+      sourceIO = BigInt("0", 16)
+      dut.io.sources #= sourceIO
+
+      assertIP(sourceIO, masterconfigs)
+      // modify
+      assertIP(sourceIO, slave1configs)
+      assertIP(sourceIO, slave2configs)
+
+      dut.clockDomain.waitRisingEdge(100)
+    }
+  }
+}
 
 class APlicTest extends SpinalSimFunSuite {
   onlyVerilator()
@@ -339,8 +547,7 @@ class APlicTest extends SpinalSimFunSuite {
     assert(((io >> id) & BigInt(1)) == value, s"$name: missmatch value = ${io} >> ($id) & 1 != (${value})")
   }
 
-  def assertIP(dut: APlicUnitFiberTest, sourceIO: BigInt, configs: ArrayBuffer[gateway]) = {
-    dut.io.sources #= sourceIO
+  def assertIP(sourceIO: BigInt, configs: ArrayBuffer[gateway]) = {
     for ((config, i) <- configs.zipWithIndex) {
       if (config.mode != sourceMode.DETACHED) {
         config.assertIP(sourceIO.testBit(i).toInt)
@@ -362,7 +569,7 @@ class APlicTest extends SpinalSimFunSuite {
     def target = if (deliveryMode) SimUInt32((eiid | (guestIndex << 12) | (hartId << 18)) & 0xFFFFFFFF)
                    else SimUInt32((iprio | (hartId << 18)) & 0xFFFFFFFF)
 
-    def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int): Unit
+    def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0): Unit
     def assertIE(): Unit
     def assertIP(io: Int): Unit
   }
@@ -370,9 +577,12 @@ class APlicTest extends SpinalSimFunSuite {
   case class inactive(id: Int, agent: tilelink.sim.MasterAgent, base: Int) extends gateway(id) {
     mode = sourceMode.INACTIVE
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 0
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x0))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x0))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.clrienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
@@ -389,9 +599,12 @@ class APlicTest extends SpinalSimFunSuite {
   case class detached(id: Int, agent: tilelink.sim.MasterAgent, base: Int) extends gateway(id) {
     mode = sourceMode.DETACHED
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 1
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x1))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x1))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.setienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
@@ -409,9 +622,12 @@ class APlicTest extends SpinalSimFunSuite {
     mode = sourceMode.EDGE1
     var reg = 0
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 1
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x4))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x4))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.setienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
@@ -433,9 +649,12 @@ class APlicTest extends SpinalSimFunSuite {
     mode = sourceMode.EDGE0
     var reg = 0
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 1
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x5))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x5))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.setienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
@@ -456,9 +675,12 @@ class APlicTest extends SpinalSimFunSuite {
   case class level1(id: Int, agent: tilelink.sim.MasterAgent, base: Int) extends gateway(id) {
     mode = sourceMode.LEVEL1
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 1
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x6))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x6))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.setienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
@@ -475,9 +697,12 @@ class APlicTest extends SpinalSimFunSuite {
   case class level0(id: Int, agent: tilelink.sim.MasterAgent, base: Int) extends gateway(id) {
     mode = sourceMode.LEVEL0
 
-    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int) = {
+    override def setMode(agent: tilelink.sim.MasterAgent, base: Int, offset: Int, childId: Int = 0) = {
       ie = 1
-      agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x7))
+      if (childId == 0)
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x7))
+      else
+        agent.putFullData(0, base + aplicmap.sourcecfgOffset + offset, SimUInt32(0x400 | childId))
       agent.putFullData(0, base + aplicmap.setienumOffset, SimUInt32(id))
       agent.putFullData(0, base + aplicmap.targetOffset + offset, target)
     }
