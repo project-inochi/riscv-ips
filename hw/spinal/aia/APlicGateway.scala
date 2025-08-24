bc7697122e4196e15367c2d71c466b024aa21595
Inochi Amaoto <inochiama@gmail.com>
2025-08-24 18:36:50 +0800

    hw: aia: aplic: remove unused comment code
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- hw/spinal/aia/APlicGateway.scala
+++ hw/spinal/aia/APlicGateway.scala
@@ -62,31 +62,3 @@ case class APlicMSIGateway(interrupts: Seq[APlicSource], delivery: Bool) extends
     APlic.doClaim(interrupts, requestStream.payload.id)
   }
 }
-
-// // reserved for old two cycle trigger
-// case class APlicMSIGateway(interrupts: Seq[APlicSource], enable: Bool) extends Area {
-//   val maxSource = (interrupts.map(_.id) ++ Seq(0)).max + 1
-//   val idWidth = log2Up(maxSource)
-
-//   val requests = interrupts.sortBy(_.id).map(_.asMSIRequest(idWidth))
-
-//   val resultRequest = RegNext(requests.reduceBalancedTree((a, b) => {
-//     val takeA = a.prioritize(b)
-//     takeA ? a | b
-//   }))
-
-//   val realRequest = resultRequest.asInstanceOf[APlicMSIRequest]
-//   val realRequestDelayed = Delay(realRequest, 1)
-
-//   val requestStream = Stream(APlicMSIRequest(widthOf(resultRequest.id)))
-
-//   val requestMask = RegNext(requestStream.ready)
-//   val requestStreamValidMask = requestMask || requestStream.valid
-
-//   requestStream.valid   := resultRequest.pending(0) && enable && !requestMask
-//   requestStream.payload := realRequest
-
-//   when (requestStream.ready && requestStream.valid) {
-//     APlic.doClaim(interrupts, requestStream.payload.id)
-//   }
-// }
